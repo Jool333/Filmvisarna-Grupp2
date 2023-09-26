208a5029ec5b6effa6ecd5b51da62b7b8ac0d076
Atthaphon <blance@hotmail.se>
2023-09-26 12:01:54 +0200

    Added Filter, Moviedetail and Chargrid
    Added those components above in src for testing out the layout.

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,18 +1,32 @@
 // Only import your sass in App (not every component)
 import "./sass/main.scss";
+import React, { useState } from 'react';
+import Filter from "./components/filter/filter";
+import ChairGrid from "./components/chairGrid/ChairGrid.Jsx";
+import MovieDetail from "./components/moviedetail/moviedetail";
 
 // Import some Bootstrap components
 import MainMenu from '@/components/MainMenu/MainMenu.jsx';
 import { Container, Row, Col } from 'react-bootstrap';
 
 export default function App() {
+
+
     return <>
         <MainMenu />
         <Container className="mt-5">
             <Row>
                 <Col>
-                    <h1>Hello!</h1>
-                    <p>There you are...</p>
+               
+               <Filter>
+            
+               </Filter>
+                <MovieDetail>
+
+                </MovieDetail>
+
+                <ChairGrid>
+                </ChairGrid>
                 </Col>
             </Row>
         </Container>
---
+++ reactapp/src/components/filter/Filter.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+
+export default function Filter()
+ {
+    const [selectedDate, setSelectedDate] = useState('');
+    const [selectedAgeRating, setSelectedAgeRating] = useState('');
+    const [filteredMovies, setFilteredMovies] = useState([]);
+
+    const moviesData = [
+        {
+            title: 'Film 1',
+            date: '2023-09-30',
+            ageRating: 'PG-13',
+            
+        },
+        {
+            title: 'Film 2',
+            date: '2023-10-05',
+            ageRating: 'R',
+            
+           
+        },
+       
+    ];
+
+
+    const filterMovies = () => {
+        const filtered = moviesData.filter(movie => {
+            if (selectedDate && movie.date !== selectedDate) {
+                return false;
+            }
+            if (selectedAgeRating && movie.ageRating !== selectedAgeRating) {
+                return false;
+            }
+            return true;
+        });
+
+        setFilteredMovies(filtered);
+    };
+
+    return (
+
+        <Container className="mt-5">
+            <Row>
+                <Col>
+        <div>
+                
+        <h2>Filtrera filmer</h2>
+        
+        <Container> <Row> <Col>    
+        
+            <label htmlFor="datePicker">Välj datum:</label>
+            <input
+                type="date"
+                id="datePicker"
+                value={selectedDate}
+                onChange={(e) => setSelectedDate(e.target.value)}
+            />
+        </Col>
+        <Col> 
+            <label htmlFor="ageRatingSelect">Välj åldersgräns:</label>
+            <select
+                id="ageRatingSelect"
+                value={selectedAgeRating}
+                onChange={(e) => setSelectedAgeRating(e.target.value)}
+            >
+                <option value="">Alla åldersgränser</option>
+                <option value="G">G</option>
+                <option value="PG">PG</option>
+                <option value="PG-13">PG-13</option>
+                <option value="R">R</option>
+                <option value="NC-17">NC-17</option>
+            </select>
+            </Col>
+            <Col>
+            
+        <Button variant= "light" onClick={filterMovies}>Filtrera</Button>
+
+        </Col>
+        </Row>
+        </Container> 
+    </div>
+    
+    <div>
+        <h2>Resultat</h2>
+        <ul>
+            {filteredMovies.map((movie, index) => (
+                <li key={index}>
+                    <h3>{movie.title}</h3>
+                    <p>Datum: {movie.date}</p>
+                    <p>Åldersgräns: {movie.ageRating}</p>
+              
+                
+                </li>
+            ))}
+        </ul>
+    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
