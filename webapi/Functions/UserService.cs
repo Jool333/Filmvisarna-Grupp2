16f3c3d8ad0f94186906fe139f88ac8994bf8b14
ahmedal2 <113178170+ahmedal2@users.noreply.github.com>
2023-10-03 21:32:36 +0200

    Skapa funktion för att skapa BokingsNummer

--- webapi/Functions/UserService.cs
+++ webapi/Functions/UserService.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace webapi.Entities
 {
     public class UserService
@@ -15,6 +17,26 @@ namespace webapi.Entities
     public bool VerifyPassword(string enteredPassword, string hashedPasswordFromDatabase)
     {
         return (enteredPassword==hashedPasswordFromDatabase);
+    }
+
+    // Funktion för att skapa uniqt bokingsnummer. 
+    public class ReservationSystem
+    {
+    private Random random = new Random();
+
+    public string GenerateReservationNumber(int length = 6)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder reservationNumber = new StringBuilder();
+
+        for (int i = 0; i < length; i++)
+        {
+            int index = random.Next(chars.Length);
+            reservationNumber.Append(chars[index]);
+        }
+
+        return reservationNumber.ToString();
+    }
     }
     }
 }
