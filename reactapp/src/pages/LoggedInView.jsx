ef5972307f8a6d080e153b3591bceddf315b798c
kheyria146 <kheyriaabderahman@gmail.com>
2023-10-04 19:58:04 +0200

    mainpage
    work with home page

--- reactapp/src/pages/LoggedInView.jsx
+++ reactapp/src/pages/LoggedInView.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import BookingDetail from '../components/BookingDetail/BookingDetail'
 
 
-function LoggedInView(){
+
+
+function BookningViewPage() {
     return (
-<div>
-    LOGGED IN VIEW
-</div>
-    );
 
+        <Container className="mt-5" style={{ background: '#CDB991' }}>
+            <Row>
+
+                <div>
+                    <h2> Aktiva Bokningar</h2>
+
+                </div>
+
+                <BookingDetail />
+
+            </Row>
+            <Row>
+
+                <div>
+                    <h2> Tidigare Bokningar</h2>
+
+                    <BookingDetail />
+
+                    <BookingDetail />
+                </div>
+
+            </Row>
+
+        </Container >
+    );
 }
-export default LoggedInView;
+
+export default BookningViewPage;
