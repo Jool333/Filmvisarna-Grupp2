9fded9495078863c4d795a0a6fe754de0392cb85
ahmedal2 <113178170+ahmedal2@users.noreply.github.com>
2023-10-04 15:14:16 +0200

    Skapat Dropdown för Logga in & Registera

--- reactapp/src/components/MainMenu/MainMenu.jsx
+++ reactapp/src/components/MainMenu/MainMenu.jsx
@@ -4,7 +4,8 @@ import {
     Nav,
     Navbar,
     Row,
-    Col
+    Col,
+    Dropdown
 } from 'react-bootstrap';
 
 import logo from '@/images/LogoFilmvisarna.webp';
@@ -20,7 +21,15 @@ function MainMenu() {
           <Container fluid >
                 <Nav.Link href="/TicketViewPage" style={{color:"black"}}>Biljetter</Nav.Link>
                 <Navbar.Brand  href="/"><img src={logo} height="80rem" /></Navbar.Brand>
-                <Button variant="outline-dark" href="/LoginViewPage"> Logga in</Button>
+                <Dropdown>
+                <Dropdown.Toggle variant="outline-dark" id="login-dropdown">
+                  Logga in
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="/LoginViewPage">Logga in</Dropdown.Item>
+                  <Dropdown.Item href="/CreateAccountPage">Registrera</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
           </Container>
       </Navbar>
   );
