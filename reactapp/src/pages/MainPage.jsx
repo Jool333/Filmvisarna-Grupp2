f3cdc3e52e0be30fc5142c15756611b389756e87
kheyria146 <kheyriaabderahman@gmail.com>
2023-10-12 00:03:06 +0200

    fix responsiv for main page

--- reactapp/src/pages/MainPage.jsx
+++ reactapp/src/pages/MainPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState , useEffect} from 'react';
-import { Container, Row, Col, Button ,Nav} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Button, Nav } from 'react-bootstrap';
 import { useOutletContext } from 'react-router-dom';
 
 import img1 from "@/assets/poster/1.jpeg"
@@ -18,103 +18,103 @@ import img12 from "@/assets/poster/12.jpeg"
 
 
 function MainPage() {
-  
-  const {moviesImport} = useOutletContext();
+
+  const { moviesImport } = useOutletContext();
   const [selectedDate, setSelectedDate] = useState('');
   const [selectedAgeRating, setSelectedAgeRating] = useState('');
   const [filteredMovies, setFilteredMovies] = useState([]);
-  
+
 
   const movies = [
     {
       title: 'Film 1',
       date: '2023-09-30',
       ageRating: 'PG',
-      img: img1, 
-      link:"/movie/1"
+      img: img1,
+      link: "/movie/1"
 
     },
     {
       title: 'Film 2',
       date: '2023-10-05',
       ageRating: 'NC-17',
-      img: img2, 
-      link:"/movie/1"
+      img: img2,
+      link: "/movie/1"
     },
     {
       title: 'Film 3',
       date: '2023-10-05',
       ageRating: 'G',
-      img: img3, 
-      link:"/movie/1"
+      img: img3,
+      link: "/movie/1"
     },
     {
       title: 'Film 4',
       date: '2023-10-05',
       ageRating: 'PG-13',
-      img: img4, 
-      link:"/movie/1"
+      img: img4,
+      link: "/movie/1"
     },
     {
       title: 'Film 5',
       date: '2023-10-05',
       ageRating: 'NC-17',
-      img:img5, 
-      link:"/movie/1"
+      img: img5,
+      link: "/movie/1"
     },
     {
       title: 'Film 6',
       date: '2023-10-05',
       ageRating: 'R',
-      img:img6, 
-      link:"/movie/1"
+      img: img6,
+      link: "/movie/1"
     },
     {
       title: 'Film 1',
       date: '2023-09-30',
       ageRating: 'NC-17',
-      img: img7, 
-      link:"/movie/1"
+      img: img7,
+      link: "/movie/1"
 
     },
     {
       title: 'Film 2',
       date: '2023-10-05',
       ageRating: 'R',
-      img: img8, 
-      link:"/movie/1"
+      img: img8,
+      link: "/movie/1"
     },
     {
       title: 'Film 3',
       date: '2023-10-05',
       ageRating: 'G',
-      img: img9, 
-      link:"/movie/1"
+      img: img9,
+      link: "/movie/1"
     },
     {
       title: 'Film 4',
       date: '2023-10-05',
       ageRating: 'PG',
-      img: img10, 
-      link:"/movie/1"
+      img: img10,
+      link: "/movie/1"
     },
     {
       title: 'Film 5',
       date: '2023-10-05',
       ageRating: 'PG-13',
-      img:img11, 
-      link:"/movie/1"
+      img: img11,
+      link: "/movie/1"
     },
     {
       title: 'Film 6',
       date: '2023-10-05',
       ageRating: 'PG-13',
-      img:img12, 
-      link:"/movie/1"
+      img: img12,
+      link: "/movie/1"
     }
 
   ];
-  
+
 
   const days = [
     '2023-09-30',
@@ -143,62 +143,63 @@ function MainPage() {
 
 
 
-  useEffect(() => {  
-    
+  useEffect(() => {
+
 
     filterMovies()
 
-   },[]);
+  }, []);
 
   return (
-    <Container >
-      <Row className='custom-background p-2'>
-        <Col>
-          <label htmlFor="dateSelect">Välj datum:</label>
-          <select
-            id="dateSelect"
-            value={selectedDate}
-            onChange={(e) => setSelectedDate(e.target.value)}
-          >
-            <option value="">Välj dag</option>
-            {days.map((day, index) => (
-              <option key={index} value={day}>
-                {day}
-              </option>
-            ))}
-          </select>
-        </Col>
-        <Col>
-          <label htmlFor="ageRatingSelect">Välj åldersgräns:</label>
-          <select
-            id="ageRatingSelect"
-            value={selectedAgeRating}
-            onChange={(e) => setSelectedAgeRating(e.target.value)}
-          >
-            <option value="">Alla åldersgränser</option>
-            <option value="G">G</option>
-            <option value="PG">PG</option>
-            <option value="PG-13">PG-13</option>
-            <option value="R">R</option>
-            <option value="NC-17">NC-17</option>
-          </select>
-        </Col>
-        <Col className='text-center'>
-          <Button variant="light" size="sm" onClick={filterMovies}>
-            Filtrera
-          </Button>
-        </Col>
+    <Container className='container-main'>
+      <Row className='filter-main'>
+        <div>
+
+          <Col className='data-container'>
+            <select
+              id="dateSelect"
+              value={selectedDate}
+              onChange={(e) => setSelectedDate(e.target.value)}
+            >
+              <option value="">Välj datum</option>
+              {days.map((day, index) => (
+                <option key={index} value={day}>
+                  {day}
+                </option>
+              ))}
+            </select>
+          </Col>
+          <Col  className='age-container'>
+            <select
+              id="ageRatingSelect"
+              value={selectedAgeRating}
+              onChange={(e) => setSelectedAgeRating(e.target.value)}
+            >
+              <option value="">Välj åldersgräns</option>
+              <option value="">Alla åldersgränser</option>
+              <option value="G">G</option>
+              <option value="PG">PG</option>
+              <option value="PG-13">PG-13</option>
+              <option value="R">R</option>
+              <option value="NC-17">NC-17</option>
+            </select>
+          </Col>
+          <Col className='filter-buuton'>
+            <Button variant="light" size="sm" onClick={filterMovies}>
+              Filtrera
+            </Button>
+          </Col>
+        </div>
+
       </Row>
       <Row>
         <Col>
           <div>
             <ul className='film-list'>
               {filteredMovies.map((movie, index) => (
-                <li key={index}  className='film-list-item' >
-                  <Nav.Link href={movie.link}>
-
-                  <img src={movie.img}  width="200px" className='poster-img' />
-                  
+                <li key={index} className='film-list-item' >
+                  <Nav.Link href={movie.link} className='nav-link-main'>
+                    <img src={movie.img} width="200px" className='poster-img' />
                   </Nav.Link>
 
                 </li>
