81d28ed1045ec6a6c2ac4f9bb1d37a59c9501534
Jool333 <jool333@live.se>
2023-10-14 13:23:24 +0200

    Fixed filter for real movies

--- reactapp/src/pages/MainPage.jsx
+++ reactapp/src/pages/MainPage.jsx
@@ -1,147 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Button, Nav } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useOutletContext } from 'react-router-dom';
 
-import img1 from "@/assets/poster/1.jpeg"
-import img2 from "@/assets/poster/2.jpeg"
-import img3 from "@/assets/poster/3.jpeg"
-import img4 from "@/assets/poster/4.jpeg"
-import img5 from "@/assets/poster/5.jpeg"
-import img6 from "@/assets/poster/6.jpeg"
-
-import img7 from "@/assets/poster/7.jpeg"
-import img8 from "@/assets/poster/8.jpeg"
-import img9 from "@/assets/poster/9.jpeg"
-import img10 from "@/assets/poster/10.jpeg"
-import img11 from "@/assets/poster/11.jpeg"
-import img12 from "@/assets/poster/12.jpeg"
-
-
 function MainPage() {
-
-  const moviesImport = useOutletContext().movies;
-  console.log(moviesImport)
-  const [selectedDate, setSelectedDate] = useState('');
-  const [selectedAgeRating, setSelectedAgeRating] = useState('');
+  const outletContext = useOutletContext();
+  const [movies, setMovies] = useState([]);
+  const [filterData, setFilterData] = useState({
+    selectedAgeRating: '',
+    selectedDate: ''
+  });
   const [filteredMovies, setFilteredMovies] = useState([]);
 
 
-  const movies = [
-    {
-      title: 'Film 1',
-      date: '2023-09-30',
-      ageRating: 'PG',
-      img: img1,
-      link: "/movie/1"
-
-    },
-    {
-      title: 'Film 2',
-      date: '2023-10-05',
-      ageRating: 'NC-17',
-      img: img2,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 3',
-      date: '2023-10-05',
-      ageRating: 'G',
-      img: img3,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 4',
-      date: '2023-10-05',
-      ageRating: 'PG-13',
-      img: img4,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 5',
-      date: '2023-10-05',
-      ageRating: 'NC-17',
-      img: img5,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 6',
-      date: '2023-10-05',
-      ageRating: 'R',
-      img: img6,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 1',
-      date: '2023-09-30',
-      ageRating: 'NC-17',
-      img: img7,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 2',
-      date: '2023-10-05',
-      ageRating: 'R',
-      img: img8,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 3',
-      date: '2023-10-05',
-      ageRating: 'G',
-      img: img9,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 4',
-      date: '2023-10-05',
-      ageRating: 'PG',
-      img: img10,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 5',
-      date: '2023-10-05',
-      ageRating: 'PG-13',
-      img: img11,
-      link: "/movie/1"
-    },
-    {
-      title: 'Film 6',
-      date: '2023-10-05',
-      ageRating: 'PG-13',
-      img: img12,
-      link: "/movie/1"
+  const fetchData = async () => {
+    try {
+      const moviesData = await outletContext.movies;
+      setMovies(moviesData);
+    } catch (error) {
+      console.error('Error fetching movies: ', error);
     }
+  };
 
-  ];
+  useEffect(() => {
+    fetchData();
+  }, [outletContext]);
 
-  const days = [
+  useEffect(() => {
+    filterMovies()
+  }, [filterData, movies]);
+
+  const screeningDate = [
     '2023-09-30',
     '2023-10-05',
   ];
 
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilterData({ ...filterData, [name]: value })
+    filterMovies()
+  };
+
   const filterMovies = () => {
-    // setFilterMode(true)
     const filtered = movies.filter(movie => {
-      if (selectedDate && movie.date !== selectedDate) {
-        return false;
+      if ((filterData.selectedDate == '' /* || movie.screenings.some(screening => screening.screeningDate === filterData.selectedDate))*/)
+        && (filterData.selectedAgeRating == '' || movie.ageLimit == filterData.selectedAgeRating)
+      ) {
+        return true;
       }
-      if (selectedAgeRating && movie.ageRating !== selectedAgeRating) {
-        return false;
-      }
-      return true;
+      return false;
 
     });
-    //console.log(filtered)
+    console.log('filtrerade filmer' + filtered)
 
     setFilteredMovies(filtered);
   };
-
-
-
-  useEffect(() => {
-    filterMovies()
-  }, []);
 
   return (
     <Container >
@@ -150,11 +62,13 @@ function MainPage() {
           <label htmlFor="dateSelect">Välj datum:</label>
           <select
             id="dateSelect"
-            value={selectedDate}
-            onChange={(e) => setSelectedDate(e.target.value)}
+            name="selectedDate"
+            value={filterData.selectedDate}
+            onChange={handleFilterChange}
+            disabled //temp until fix
           >
-            <option value="">Välj dag</option>
-            {days.map((day, index) => (
+            <option value="">Alla dagar</option>
+            {screeningDate.map((day, index) => (
               <option key={index} value={day}>
                 {day}
               </option>
@@ -165,8 +79,9 @@ function MainPage() {
           <label htmlFor="ageRatingSelect">Välj åldersgräns:</label>
           <select
             id="ageRatingSelect"
-            value={selectedAgeRating}
-            onChange={(e) => setSelectedAgeRating(e.target.value)}
+            name="selectedAgeRating"
+            value={filterData.selectedAgeRating}
+            onChange={handleFilterChange}
           >
             <option value="">Alla åldersgränser</option>
             <option value="G">G</option>
@@ -176,36 +91,18 @@ function MainPage() {
             <option value="NC-17">NC-17</option>
           </select>
         </Col>
-        <Col className='text-center'>
-          <Button variant="light" size="sm" onClick={filterMovies}>
-            Filtrera
-          </Button>
-        </Col>
       </Row>
       <Row>
         <Col>
-          <div>{/*
+          <div>
             <ul className='film-list'>
-              {filteredMovies.map((movie, index) => (
-                <li key={index} className='film-list-item' >
-                  <Nav.Link href={movie.link}>
-
-                    <img src={movie.img} width="200px" className='poster-img' />
-
-                  </Nav.Link>
-
-                </li>
-              ))}
-              </ul>*/}
-
-            <ul className='film-list'>
-              {moviesImport.map((movie) => (
+              {filteredMovies.map((movie) => (
                 <li key={movie.id} className='film-list-item' >
-                  <Nav.Link href={'/movie/' + [movie.id]}>
+                  <a href={'/movie/' + [movie.id]}>
 
                     <img src={movie.imgUrl} width="200px" className='poster-img' />
 
-                  </Nav.Link>
+                  </a>
 
                 </li>
               ))}
