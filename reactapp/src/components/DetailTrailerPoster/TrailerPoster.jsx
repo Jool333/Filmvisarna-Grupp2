75057675ca8e5e0ac5753a3db3db3efc78849e4c
Jool333 <jool333@live.se>
2023-10-16 10:05:27 +0200

    Add func for get chosen movie data, used in child

--- reactapp/src/components/DetailTrailerPoster/TrailerPoster.jsx
+++ reactapp/src/components/DetailTrailerPoster/TrailerPoster.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
-const movie = {
-    embedUrl: "U1fu_sA7XhE",
-    imgUrl: "https://images.photowall.com/products/51078/movie-poster-jaws.jpg?h=699&q=85"
-};
+function TrailerPoster({ chosenMovie }) {
+    const [movie, setMovie] = useState('');
 
-function TrailerPoster() {
+    const fetchData = async () => {
+        try {
+            const movieData = await chosenMovie;
+            setMovie(movieData);
+        } catch (error) {
+            console.error('Error fetching movies: ', error);
+        }
+    };
 
+    useEffect(() => {
+        if (chosenMovie)
+            fetchData();
+    }, [chosenMovie]);
+
+
+    console.log("tp" + movie.trailerUrl)
     return (
         <Container className='mb-5 w-100' >
             <Row >
                 <Col xs={12} lg={9} className="d-flex justify-content-center align-items-center w-70">
                     <iframe className="trailer"
-                        src={"https://www.youtube.com/embed/" + `${movie.embedUrl}`}
+                        src={"https://www.youtube.com/embed/" + `${movie.trailerUrl}`}
                     />
                 </Col>
                 <Col xs={12} lg={3} className="d-flex justify-content-center align-items-center">
--- reactapp/src/pages/MovieDetailPage.jsx
+++ reactapp/src/pages/MovieDetailPage.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 import TrailerPoster from "../components/DetailTrailerPoster/TrailerPoster";
 import MovieDetail from "../components/moviedetail/MovieDetail";
-
+import { get } from '../ApiConnection.jsx';
 
 function MovieDetails() {
+  const [movie, setMovie] = useState(null);
+  const movieId = useParams().id;
+  console.log(movieId)
+
+  const fetchData = async () => {
+    try {
+      const moviesData = await get(`movies/${movieId}`);
+      console.log(moviesData)
+      setMovie(moviesData);
+    } catch (error) {
+      console.error('Error fetching movies: ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [movieId]);
+
   return (
     <Container className="mt-3">
       <Row>
         <Col>
-          <TrailerPoster />
+          <TrailerPoster chosenMovie={movie} />
         </Col>
       </Row>
       <Row>
         <Col>
-          <MovieDetail />
+          <MovieDetail chosenMovie={movie} />
         </Col>
       </Row>
     </Container>
