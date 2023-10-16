000411aace5bf2e089befc1f570768423c10af77
Jool333 <jool333@live.se>
2023-10-16 14:16:08 +0200

    fix salt static

--- webapi/Functions/UserService.cs
+++ webapi/Functions/UserService.cs
@@ -6,7 +6,7 @@ namespace webapi.Functions
 {
     public class UserService
     {
-        private string salt = GetSalt();
+        private static string salt = GetSalt();
         public static string HashPassword(string password)
         {
             return Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(password + salt)));
