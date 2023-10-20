fbca03652c25026c69f77d77d3042cb97856499a
ahmedal2 <113178170+ahmedal2@users.noreply.github.com>
2023-10-21 00:29:50 +0200

    Hämtad screeningar från db för att kunna routa vidare via Id

--- reactapp/src/pages/BookingViewPage.jsx
+++ reactapp/src/pages/BookingViewPage.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Col } from 'react-bootstrap';
 import SeatsGrid from '../components/seatsGrid/SeatsGrid';
-import { get } from '@/Apiconnection.jsx'
+import { get } from '@/Apiconnection.jsx';
+import { useParams } from 'react-router-dom';
 
 function BookingViewPage() {
   const [seats, setSeats] = useState(null);
+
+  const { screeningId } = useParams();
+  const [screening, setScreening] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      setScreening(await (await fetch('/api/screenings/' + screeningId)).json());
+    })();
+  }, []);
+
 
   useEffect(() => {
     (async () => {
@@ -22,7 +33,7 @@ function BookingViewPage() {
     console.log("isContinueEnabled:", isContinueEnabled);
   };
 
-  return (
+  return !screening ? null : (
     <Container>
       <Col className=' text-light'>
         <div className="d-flex justify-content-between">
@@ -32,7 +43,7 @@ function BookingViewPage() {
         <hr />
       </Col>
       <Col className='d-flex align-items-center justify-content-center pt-3' >
-        <SeatsGrid onSeatsSelected={handleSeatsSelected} />
+        <SeatsGrid onSeatsSelected={handleSeatsSelected} screening={screening} />
       </Col>
 
     </Container>
