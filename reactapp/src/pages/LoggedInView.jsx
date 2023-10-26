83295868f227d5bdb8c1cc99f28bc66ea5084d0a
kheyria146 <kheyriaabderahman@gmail.com>
2023-10-26 16:06:39 +0200

    Revert "add function for get bookings data from backend"
    This reverts commit b9b267d95ee3559abaa4acf8c89a71d5ac4cd670.

--- reactapp/src/pages/LoggedInView.jsx
+++ reactapp/src/pages/LoggedInView.jsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from 'react'
-
+import React from 'react'
 import { Container, Row } from 'react-bootstrap';
 import ActiveBookingDetail from '../components/BookingDetail/ActiveBookingDetail'
 import InActiveBookingDetail from '../components/BookingDetail/InActiveBookingDetail';
-import { get } from '../ApiConnection.jsx';
 
 
 
 
 function LoggedInView() {
-    const [bookings, setBookings] = useState([]);
-    useEffect(() => {
-        (async () => {
-            try {
-
-                //Jag vill dubbelkolla om du vill hämta alla data för bookningar i loggedinView och sedan filter de aktiva och de tidigare bookningar och skicka dessa data till deras komponetenr
-                // Hur kan vi får UserId här om inloggning inte funkar än som ska?
-
-                const bookingsData = await get(`bookings/${UserId}/`);
-
-                console.log(bookingsData)
-                setBookings(bookingsData)
-            } catch (error) {
-                console.error('Error fetching movies: ', error);
-            }
-        })();
-    }, []);
-
     return (
 
         <Container className="container-loggedInView custom-background p-3 rounded-5">
