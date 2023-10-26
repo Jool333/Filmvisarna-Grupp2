e23e502821c5c36aee9e340818955b7c607e69ea
kheyria146 <kheyriaabderahman@gmail.com>
2023-10-26 16:06:05 +0200

    Revert "Delete ViewingsController.cs"
    This reverts commit 1cbd6358de170ccd93bcdaba03016646004a488a.

---
+++ webapi/Controllers/ViewingsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("api/viewings")]
+    public class ViewingsController : ControllerBase
+    {
+
+    }
+}
